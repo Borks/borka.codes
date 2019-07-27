0511e40d1b17d34d78a9dd91042942523805b098
Borka Martin Orlov <borka.orlov@gmail.com>
2019-07-27 15:00:16 +0300

    Switched Skills to another component

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -8,7 +8,7 @@ export default function Subscribe() {
           <div className="col-md-10 col-lg-8 mx-auto text-center">
             <i className="far fa-paper-plane fa-2x mb-2 text-white"></i>
             <h2 className="text-white mb-5">Send me a message</h2>
-            <p><i>PS: That's not me on the pictures, maybe one day</i></p>
+            <p><i>PS: That's not me on the pictures, not yet.</i></p>
 
             <form>
               <div className='form-group row'>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,7 @@ import Footer from '../components/Footer';
 import SocialLinks from '../components/SocialLinks';
 import Subscribe from '../components/Subscribe';
 import Header from '../components/Header';
-
-import arrow from '../assets/images/arrow.png';
-import bracket from '../assets/images/bracket.png';
-import db_logo from '../assets/images/db-logo.png';
-import django_logo from '../assets/images/django-logo.png';
-import docker_logo from '../assets/images/docker-logo.png';
-import gatsbyjs_logo from '../assets/images/gatsbyjs-logo.png';
-import laravel_logo from '../assets/images/laravel-logo.png';
-import nodejs_logo from '../assets/images/nodejs-logo.png';
-import python_logo from '../assets/images/python-logo.png';
-import react_logo from '../assets/images/react-logo.png';
-import scala_logo from '../assets/images/scala-logo.png';
-import vuejs_logo from '../assets/images/vuejs-logo.png';
-import angular_logo from '../assets/images/angular-logo.png';
+import Skills from '../components/Skills';
 
 import ffone from '../assets/images/ffone.jpg';
 import aavikuemand from '../assets/images/aavikuemand.png';
@@ -42,41 +29,7 @@ const IndexPage = () => (
       </div>
     </header>
 
-    <section id="about" className="about-section text-center">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8 mx-auto">
-            <h2 className="text-white mb-4">End-to-end software solutions</h2>
-            <p className="text-white-50">
-
-            </p>
-          </div>
-        </div>
-
-        <div className='skills-container'>
-            <img src={db_logo} className="img-fluid about-skills-image" alt="" />
-            <img src={arrow} className="img-fluid about-skills-image arrow" alt="" />
-            <div class='upright-container'>
-                <img src={django_logo} className="img-fluid about-skills-image" alt="" />
-                <img src={python_logo} className="img-fluid about-skills-image" alt="" />
-            </div>
-            <div class='upright-container'>
-                <img src={laravel_logo} className="img-fluid about-skills-image" alt="" />
-                <img src={nodejs_logo} className="img-fluid about-skills-image" alt="" />
-            </div>
-            <img src={scala_logo} className="img-fluid about-skills-image" alt="" />
-            <img src={arrow} className="img-fluid about-skills-image arrow" alt="" />
-            <img src={vuejs_logo} className="img-fluid about-skills-image" alt="" />
-            <img src={react_logo} className="img-fluid about-skills-image" alt="" />
-            <img src={angular_logo} className="img-fluid about-skills-image" alt="" />
-            <img src={gatsbyjs_logo} className="img-fluid about-skills-image" alt="" />
-            </div>
-        <div>
-            <img src={bracket} className="img-fluid about-skills-image bracket-image" alt="" />
-        </div>
-        <img src={docker_logo} className="img-fluid about-skills-image" alt="" />
-        </div>
-    </section>
+    <Skills />
 
     <section id="projects" className="projects-section bg-light">
       <div className="container">
