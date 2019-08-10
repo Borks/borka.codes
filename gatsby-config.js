32cc82b0d3f0ceabdad3b30bed26883a696bb77c
Borka Martin Orlov <borka.orlov@gmail.com>
2019-08-10 18:36:46 +0300

    Added Gtag manager.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,16 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-79040314-3`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 };
